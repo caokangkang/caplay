65db85698cf9d30c921c8b734454767491203b11
caokk <hkd_ckk@163.com>
2017-06-08 18:26:09 +0800

    add muisc.html
    添加音乐页面(未完成)

---
+++ script/data.js
@@ -0,0 +1,22 @@
+var audio_list = [
+	{
+		'src' : 'http://www.puzzle-idea.com/m/caokangkang/canplay/music/Baby%20Song.mp3',
+		'title' : 'Boby Song',
+		'singer' : '陈奕迅',
+	},
+	{
+		'src' : "http://www.puzzle-idea.com/m/caokangkang/canplay/music/The%20Ocean.mp3",
+		'title' : 'The Ocean',
+		'singer' : 'J.Fla'
+	},
+	{
+		'src' : 'http://www.puzzle-idea.com/m/caokangkang/canplay/music/Ngau_Hung.mp3',
+		'title' : 'Ngau Hung',
+		'singer' : 'Hoaprox/Newton-'
+	},
+	{
+		'src' : 'http://www.puzzle-idea.com/m/caokangkang/canplay/music/Reason.mp3',
+		'title' : 'Reason',
+		'singer' : 'Axero/Jimmy Magardeau'
+	}
+];
